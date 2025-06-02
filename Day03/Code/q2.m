ffab38a4ff89ba9b4cf5ca23a8a9889208ba81ac
ushan damindu kumara <ushandamindukumara@gmail.com>
2025-06-03 00:25:32 +0530

    add day3

---
+++ Day03/Code/q2.m
@@ -0,0 +1,31 @@
+N = [2  4  -7  -4; 0  0  3  -2; 0  -8  -0  -3; 0  -6  7  1]
+M = [4  -2  -4  7; 1  5  -3  2; 6  -8  -5  -6; -7  3  0  1]
+
+ M+N
+ N-M
+ N*M
+ N*M
+
+ C = [0  3 ; 5 1;  0  4] 
+ D = [1  0 2  ;7 1  0]
+ C*D
+ D*C
+ N^2
+ N^2-M^2
+ 3*N-2*M
+ inv(M)
+ inv(D)
+ N'
+ transpose(M)
+ flipud(M)
+ (M*N)'
+ fliplr(M)
+ rot90(M)
+ reshape(N,8,2)
+ M.*N
+ M.^2
+ 1./M
+ s=rand(3)
+ s=floor(rand(3)*100)
+ r=rand(3,5)*100
+ r=floor(rand(3,5)*100)
